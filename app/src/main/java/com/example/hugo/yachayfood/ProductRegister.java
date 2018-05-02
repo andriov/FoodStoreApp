e4e8b5139e90fe69e0375a2dc84ffaf3e3543f9d
andriov <andriov@hotmail.com>
2018-05-02 01:36:41 -0500

    Creation of second request: insert product

--- app/src/main/java/com/example/hugo/yachayfood/ProductRegister.java
+++ app/src/main/java/com/example/hugo/yachayfood/ProductRegister.java
@@ -1,19 +1,23 @@
 package com.example.hugo.yachayfood;
 
 import android.content.Intent;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.Toast;
 
 import com.example.foodstoreexception.Exceptions;
 
-import java.util.ArrayList;
+import java.io.IOException;
 
 public class ProductRegister extends AppCompatActivity {
 
+    //Creation of connection
+    private OpenConnection openConnection = new OpenConnection();
     //Button to registe the product
     private Button btnRegister;
     //EditText to insert the name of the product
@@ -25,19 +29,31 @@ public class ProductRegister extends AppCompatActivity {
     //EditText to registe the cost of the product
     private EditText productCost;
     //Object that work with the exceptions of productName, productDescription, productStock and productCost
+    private RadioButton rbtBreakFast, rbtnLunch, rbtnFastFoot;
+    //Integer of found button
+    private int rbtnanswer = 1;
+    //Excepcions obtained by imported module created by us
     private Exceptions exception = new Exceptions();
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_register);
+        //Let connection in main thread
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
         //Insertion of the references(activity_product_register) of the button and EditText created as global variables
         productName = (EditText) findViewById(R.id.productName);
         productDescription = (EditText) findViewById(R.id.productDescription);
         productStock = (EditText) findViewById(R.id.productStock);
         productCost = (EditText) findViewById(R.id.productCost);
         btnRegister = (Button) findViewById(R.id.btnRegister);
+        //Insertion of the references to radio buttons
+        rbtBreakFast = (RadioButton) findViewById(R.id.rbtnDesayuno);
+        rbtnLunch = (RadioButton) findViewById(R.id.rbtnAlmuerzo);
+        rbtnFastFoot = (RadioButton) findViewById(R.id.rbtnComidaRapida);
         //Insertion of the product if all the values are inserted correctly
         btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -50,7 +66,7 @@ public class ProductRegister extends AppCompatActivity {
                             public void run() {
                                 try {
                                     //After the insertion the app go to seller profile
-                                    Intent i=new Intent(getApplicationContext(),MainActivity.class);
+                                    Intent i=new Intent(getApplicationContext(),FragmentSellerProfile.class);
                                     //If the name is greater than 30 charcters or is a number we obtain a exception
                                     exception.stinException("name",productName.getText(),30);
                                     //If the name is greater than 240 charcters or is a number we obtain a exception
@@ -59,6 +75,10 @@ public class ProductRegister extends AppCompatActivity {
                                     exception.stinException("stock",productStock.getText());
                                     //If the cost is not a double number we obtain a exception
                                     exception.stinException("cost",productCost.getText());
+                                    validation();
+                                    openConnection.insertarDatos("{\"category\":\""+rbtnanswer+"\",\"name\":\""+productName.getText().toString()
+                                            +"\",\"description\":\""+productDescription.getText().toString()+"\",\"stock\":"+productStock.getText().toString()
+                                            +",\"price\":\""+productCost.getText().toString()+"\"}");
                                     startActivity(i);
                                 }catch (Exceptions e){
                                     //Print the exactly error for developer
@@ -78,4 +98,17 @@ public class ProductRegister extends AppCompatActivity {
             }
         });
     }
+
+    private void validation(){
+        if(rbtBreakFast.isChecked()){
+            rbtnanswer=1;
+        }
+        if(rbtnLunch.isChecked()){
+            rbtnanswer=2;
+        }
+        if(rbtnFastFoot.isChecked()){
+            rbtnanswer=3;
+        }
+    }
+
 }
